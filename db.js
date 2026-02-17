6359fa67b39e4c33b4e6d70cafda566fcb54780c
AntonioGaldon <antoniogaldoncarrion@gmail.com>
2026-02-17 11:49:09 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
-// db.js
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || "antoniogaldoncarrion",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "restaurante",
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 5432,
+        user: process.env.DB_USER || "antoniogaldoncarrion",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || "restaurante",
+      }
+);
 
 module.exports = pool;
-
