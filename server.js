122551422ac4fa46bb89c309e776ef59759e3b94
AntonioGaldon <antoniogaldoncarrion@gmail.com>
2026-02-19 08:41:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "admin.html"))
 app.get("/restaurante.html", (req, res) => res.sendFile(path.join(__dirname, "restaurante.html")));
 app.get("/restaurante", (req, res) => res.sendFile(path.join(__dirname, "restaurante.html")));
 app.get("/haptic.js", (req, res) => res.sendFile(path.join(__dirname, "haptic.js")));
+app.get("/pull-to-refresh.js", (req, res) => res.sendFile(path.join(__dirname, "pull-to-refresh.js")));
 
 
 
